50a9124f445c5b0efc737819534cba2a9a84b0c4
Felipepinales14 <38191955+Felipepinales14@users.noreply.github.com>
2018-04-10 19:13:50 -0400

    Subida Final del trabajo

---
+++ Trabajo/Trabajo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Trabajo
+{
+    partial class Trabajo
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.txtpresion = new System.Windows.Forms.TextBox();
+            this.txttemperatura = new System.Windows.Forms.TextBox();
+            this.txtvolumen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(361, 30);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(361, 82);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpiar.TabIndex = 0;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // txtpresion
+            // 
+            this.txtpresion.Location = new System.Drawing.Point(180, 30);
+            this.txtpresion.Name = "txtpresion";
+            this.txtpresion.Size = new System.Drawing.Size(100, 20);
+            this.txtpresion.TabIndex = 1;
+            // 
+            // txttemperatura
+            // 
+            this.txttemperatura.Location = new System.Drawing.Point(180, 85);
+            this.txttemperatura.Name = "txttemperatura";
+            this.txttemperatura.Size = new System.Drawing.Size(100, 20);
+            this.txttemperatura.TabIndex = 1;
+            // 
+            // txtvolumen
+            // 
+            this.txtvolumen.Location = new System.Drawing.Point(180, 59);
+            this.txtvolumen.Name = "txtvolumen";
+            this.txtvolumen.Size = new System.Drawing.Size(100, 20);
+            this.txtvolumen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese la presion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese el Volumen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ingrese la temperatura";
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(180, 165);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(256, 20);
+            this.txtresultado.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Resultado";
+            // 
+            // Trabajo
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 206);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtvolumen);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txttemperatura);
+            this.Controls.Add(this.txtpresion);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Trabajo";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.TextBox txtpresion;
+        private System.Windows.Forms.TextBox txttemperatura;
+        private System.Windows.Forms.TextBox txtvolumen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Trabajo/Trabajo/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo
+{
+    public partial class Trabajo : Form
+    {
+        public Trabajo()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+             //Declarando las variables
+            double presion;
+            double tempreratura;
+            double volumen;
+            double resultado;
+            
+
+            //comvirtiendolas en txt
+
+
+            presion = double.Parse(txtpresion.Text);
+            tempreratura = double.Parse(txttemperatura.Text);
+            volumen = double.Parse(txtvolumen.Text);
+
+            //operacion
+
+
+            resultado = (presion * volumen) / (0.37 * (tempreratura + 460));
+
+            //Salida
+
+            txtresultado.Text = resultado.ToString();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            //limpiando
+
+            txtpresion.Clear();
+            txtresultado.Clear();
+            txttemperatura.Clear();
+            txtvolumen.Clear();
+            txtpresion.Focus();
+        }
+    }
+}
